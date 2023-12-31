898cc81ac563016f923c8ec2c66d28394534706b
nohanaga <nohanaga@microsoft.com>
2023-12-31 11:03:29 +0900

    remove openai

--- aoai-rag/plugins/restaurant-reservation-plugin/main.py
+++ aoai-rag/plugins/restaurant-reservation-plugin/main.py
@@ -1,25 +1,11 @@
 import json
 import logging
 
-import openai
 import quart
 import quart_cors
 from quart import request
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
-
-# こっちで変換するのもありかも
-def get_isodatetime(strdatetime):
-    prompt = """以下の日時を ISO 8601 に変換しなさい。年が無い場合は、2023年を使いなさい。
-    Datetime: {}
-    """
-    res = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt.format(strdatetime),
-        max_tokens=2048,
-        temperature=0
-    )
-    return(res.choices[0].text)
 
 # Keep track of todo's. Does not persist if Python session is restarted.
 _TODOS = {}
@@ -29,7 +15,7 @@ async def reserve_restaurant():
     request = await quart.request.get_json(force=True)
     logging.info(request)
     datetime = request['datetime']
-    logging.info(get_isodatetime(datetime))
+    logging.info("datetime: {}".format(datetime))
 
     return quart.Response(response='OK', status=200)
 
@@ -39,7 +25,7 @@ async def search():
     query = request.args.get("q")
     datetime = request.args.get("datetime")
     logging.info(query, datetime)
-    logging.info(get_isodatetime(datetime))
+
     list_rest = [{"cafename": "カフェかば殿", "2023/07/01 18:00-19:00": "空き"}]
 
     list_search = list(filter(lambda item : item['cafename'] == query, list_rest))
